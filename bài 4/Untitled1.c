73fb009e636cdf5219455badb7e59b4e6fdfe25c
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-11-27 07:35:05 +0700

    upload bài 4

---
+++ bài 4/Untitled1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a = 5;
+	int p = a * 4;
+	printf("chu vi hinh vuong la:%d \n", p);
+	int s = a * a;
+	printf("dien tich hinh vuong la:%d \n", s);
+	return 0;
+}
