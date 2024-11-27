cda6775cd9c5470d5f3d15ba5568beb5716f7597
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-11-27 07:10:40 +0700

    upload bài 1

---
+++ bài 1/Untitled1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+	int age = 20; //kieu int la kieu so nguyen dung de lu tru so nguyen
+	float height = 1.75; //kieu float la kieu so thuc dung de lu tru so co dau thap phan
+	double weight = 70.5; //kieu du lieu double la kieu so thuc voi do chinh xac cao hon float
+	char grade = "B"; //kieu char la kieu ky tu dung de luu tru mot ky tu duy nhat
+	return 0;
+}
