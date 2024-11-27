d4b00c81beeac922a748b3b179a74c0a967ea855
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-11-27 07:37:13 +0700

    upload bài 5

---
+++ bài 5/Untitled1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int a = 20;
+	int b = 30;
+	int p = (a + b) * 2;
+	printf("chu vi cua hinh chu nhat la:%d \n", p);
+	int s = a * b;
+	printf("dien tich cua hinh chu nhat la:%d \n", s);
+	return 0;
+}
