b1da34d7681cc5359bf450a863b2c717952521d5
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-11-27 07:38:30 +0700

    upload bài 6

---
+++ bài 6/Untitled1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float pi = 3.14;
+	float r = 6;
+    float c = r * 2 * 3.14;
+	printf("chu vi hinh tron la:%f \n", c);
+	float s = r * r * 3.14;
+	printf("dien tich hinh tron la:%f \n", s);
+	return 0;
+}
