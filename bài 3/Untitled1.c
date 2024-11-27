6643382ed60c1bf43becee37ba483c1220b01aef
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-11-27 07:34:10 +0700

    upload bài 3

---
+++ bài 3/Untitled1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int sum1 = 30;
+	int sum2 = 40;
+	int total = sum2 + sum1;
+	printf("tong cua bieu thuc tren la:%d \n", total);
+	int hieu = sum2 - sum1;
+	printf("hieu cua bieu thuc tren la:%d \n", hieu);
+	int tich = sum2 * sum1;
+	printf("tich cua bieu thuc tren la:%d \n", tich);
+	int thuong = sum2 / sum1;
+	printf("thuong cua bieu thuc tren la:%d \n", thuong);	
+	return 0;
+}
